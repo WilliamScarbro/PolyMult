676b394169625e541e59ba03802d83a4ff6cbc68
WilliamScarbro <porthos1123@gmail.com>
2022-08-30 11:27:16 -0600

    corrent implementation of phi-inv and simple test suite

---
+++ maude/Multiply/bin/maude-test.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+function usage {
+  echo "Usage: maude-tests.sh"
+  echo "  -m <FILE> : maude file "
+  echo "  -t <FILE> : test file "
+  echo "  -i        : initialize test file"
+  echo "  (implied) <FILE> : correct result file in \$MAUDE_HOME/correct/\$t"
+}
+
+if [[ -z $MAUDE_HOME ]]; then
+  echo "MAUDE_HOME must be set"
+  exit 1
+fi
+
+mh=$MAUDE_HOME
+
+
+while getopts im:t: flag
+do
+  case ${flag} in
+    m) m="${OPTARG}";;
+    t) t="${OPTARG}";;
+    i) i=i;;
+    ?) usage; echo "Error: unknown option '$flag'"; exit 1;;
+  esac
+done
+
+if [[ -z $m ]] || [[ -z $t ]] ; then
+  usage
+  echo "Error: missing required option"
+  exit 1;
+fi
+
+
+if [[ ! -f $mh$m ]] || [[ ! -f $mh$t ]]; then
+  usage
+  echo "Error: one of inputs is not a file"
+  echo "> $mh$m"
+  echo "> $mh$t"
+  exit 1;
+fi
+
+tn=${t##*/}
+c=$mh/correct/$tn
+
+if [[ ! -f $c ]] && [[ -z $i ]]; then
+  usage
+  echo "Error: correct file '$c' does not exist"
+  exit 1;
+fi
+
+th=/tmp/maude-test
+
+mkdir -p $th
+
+maude-pipe.sh -m $m < $mh$t > $th/$tn-result 
+
+if [[ "$i" == "i" ]]; then
+  cat $th/$tn-result > $c
+  echo "Populated correct file $c"
+  exit 0;
+fi
+
+diff $th/$tn-result $c | tee $th/$tn-diff
+
+if [[ ! -z "`cat $th/$tn-diff`" ]]; then
+  echo "Test fail on input $mh$t" 1>&2
+  exit 1;
+fi
